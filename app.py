e41bd4f5bd60386f10f32ed2e75b77a90af82778
123Stan-The-Man123 <Molsas2020@gmail.com>
2024-08-08 13:43:49 +0100

    Implemented basic  homepage

--- app.py
+++ app.py
@@ -5,9 +5,4 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-
-        return render_template("name.html", name=name)
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
